60996edad2cfbaa20b458f227f969c071b3c42c8
mahidhasan96 <mahidhasan1122@gmail.com>
2020-08-11 10:48:49 +0600

    first commit

---
+++ window.js
@@ -0,0 +1,9 @@
+ const name = "mahid";
+ function add(num1,num2){
+    result = num1 + num2;
+    console.log('inside the name', name);
+    return result;
+}
+console.log('out site name', name);
+const output = add(100,200);
+console.log(output);
