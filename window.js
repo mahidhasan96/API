7508b2d4b3480eb0bb582f46ce7e4ed740d29744
mahidhasan96 <mahidhasan1122@gmail.com>
2020-08-11 11:06:06 +0600

    first commit

--- window.js
+++ window.js
@@ -2,7 +2,11 @@
  function add(num1,num2){
     result = num1 + num2;
     console.log('inside the name', name);
-    return result;
+    function double(num){
+        return double * 2;
+    }
+    let total = double(result);
+    return total;
 }
 console.log('out site name', name);
 const output = add(100,200);
