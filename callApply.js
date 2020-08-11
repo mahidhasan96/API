d19b445aec38e5b18ae62cc7de6649a27b16ef86
mahidhasan96 <mahidhasan1122@gmail.com>
2020-08-11 10:15:41 +0600

    adding bind

--- callApply.js
+++ callApply.js
@@ -6,10 +6,30 @@ const normalPerson = {
         console.log(this.firstName, this.lastName);
     },
     chargeBill:function(amount){
+        console.log(this);
         this.salary = this.salary - amount;
         return this.salary;
     }
 }
-normalPerson.chargeBill(5000);
-normalPerson.chargeBill(500);
-console.log(normalPerson.salary)
+const heroPerson = {
+    firstName: "baby",
+    lastName: "babu vai",
+    salary: 25000,
+}
+const friendlyPerson = {
+    firstName: "sanjuda",
+    lastName: "jakirul",
+    salary: 25000,
+}
+//normalPerson.chargeBill();
+const heroPersonBill = normalPerson.chargeBill.bind(heroPerson)
+heroPersonBill(2000);
+heroPersonBill(3000);
+console.log(heroPerson.salary);
+
+const friendlyPersonBill = normalPerson.chargeBill.bind(friendlyPerson);
+friendlyPersonBill(4000);
+friendlyPersonBill(1000);
+console.log(friendlyPerson.salary);
+console.log(normalPerson.salary);
+
