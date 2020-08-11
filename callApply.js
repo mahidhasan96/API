a7d406831da29c98231e4049bfb8e4eac9969d13
mahidhasan96 <mahidhasan1122@gmail.com>
2020-08-11 10:34:24 +0600

    adding call and apply

--- callApply.js
+++ callApply.js
@@ -5,9 +5,9 @@ const normalPerson = {
     fullName: function(){
         console.log(this.firstName, this.lastName);
     },
-    chargeBill:function(amount){
+    chargeBill:function(amount, tisk, tasx){
         console.log(this);
-        this.salary = this.salary - amount;
+        this.salary = this.salary - amount - tisk - tasx;
         return this.salary;
     }
 }
@@ -16,20 +16,32 @@ const heroPerson = {
     lastName: "babu vai",
     salary: 25000,
 }
-const friendlyPerson = {
-    firstName: "sanjuda",
-    lastName: "jakirul",
-    salary: 25000,
+const firendPerson = {
+    firstName:"mahid",
+    lastName: "hasan",
+    salary: 14000,
 }
 //normalPerson.chargeBill();
-const heroPersonBill = normalPerson.chargeBill.bind(heroPerson)
-heroPersonBill(2000);
-heroPersonBill(3000);
+// const heroPersonBill = normalPerson.chargeBill.bind(heroPerson)
+// heroPersonBill(2000);
+// heroPersonBill(3000);
+// console.log(heroPerson.salary);
+
+// const friendlyPersonBill = normalPerson.chargeBill.bind(friendlyPerson);
+// friendlyPersonBill(4000);
+// friendlyPersonBill(1000);
+// console.log(friendlyPerson.salary);
+// console.log(normalPerson.salary);
+
+// normalPerson.chargeBill.call(heroPerson,900, 50, 50);
+// normalPerson.chargeBill.call(heroPerson,1000, 100, 150);
+// console.log(heroPerson.salary);
+
+// normalPerson.chargeBill.call(firendPerson, 900, 400, 300);
+// console.log(firendPerson.salary);
+// console.log(normalPerson.salary);
+
+normalPerson.chargeBill.apply(heroPerson,[1000,400,300]);
+normalPerson.chargeBill.apply(heroPerson,[4000,400,300]);
 console.log(heroPerson.salary);
-
-const friendlyPersonBill = normalPerson.chargeBill.bind(friendlyPerson);
-friendlyPersonBill(4000);
-friendlyPersonBill(1000);
-console.log(friendlyPerson.salary);
 console.log(normalPerson.salary);
-
